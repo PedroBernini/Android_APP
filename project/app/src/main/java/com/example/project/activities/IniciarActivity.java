59ae36c13ae6d4ca59cf23ba8730c098582d1496
PedroBernini <peeeh.henrique@gmail.com>
2019-11-22 01:08:07 -0300

    Finalização Banco de Dados

--- project/app/src/main/java/com/example/project/activities/IniciarActivity.java
+++ project/app/src/main/java/com/example/project/activities/IniciarActivity.java
@@ -1,5 +1,6 @@
 package com.example.project.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +18,8 @@ public class IniciarActivity extends AppCompatActivity {
     }
 
     public void clickEntrar(View view) {
-        this.onBackPressed();
+        finish();
+        Intent intent = new Intent(this, MainActivity.class);
+        this.startActivity(intent);
     }
 }
